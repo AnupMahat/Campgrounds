e7554c456265d39cb23032a5363d31c91938b238
Anup Mahat <mahatanup8@gmail.com>
2020-08-05 09:42:53 +0000

    database connected

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes		 = require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelpcamp",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://anoop:Letmein01#@cluster0.xhjoy.mongodb.net/test",{useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+ "/public"));//for css
