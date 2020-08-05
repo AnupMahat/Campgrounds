933ed4b89482ef3766c6037af0f61b1e1a2556e6
Anup Mahat <mahatanup8@gmail.com>
2020-08-05 14:51:47 +0000

    updated mongoose connect

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes		 = require("./routes/index")
 
-mongoose.connect("mongodb+srv://anoop:Letmein01#@cluster0.xhjoy.mongodb.net/test",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://anoop:Letmein01@cluster0.xhjoy.mongodb.net/yelpCamp?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+ "/public"));//for css
